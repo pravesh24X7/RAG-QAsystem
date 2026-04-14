9e4bf8f3a6b35b9cabe469c8914914e51aa7eb4c
Pravesh Srivastava <papsri.2015@gmail.com>
2026-04-14 15:56:21 +0530

    changes regarding prompt folder
    prompt_path updated from prompt.json to prompts.

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ class Settings:
     VECTOR_DB_PATH = os.getenv("VECTOR_DB_PATH", "./vector_store/")
     MODEL_NAME = "meta-llama/llama-4-scout-17b-16e-instruct"
     DATA_PATH = os.getenv("DATA_PATH", "./data/")
-    PROMPT_PATH = os.getenv("PROMPT_PATH", "./prompt.json")
+    PROMPT_PATH = os.getenv("PROMPT_PATH", "./prompts")
 
 
 settings = Settings()
